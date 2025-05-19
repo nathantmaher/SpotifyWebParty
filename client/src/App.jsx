ac7b1e2860e72b3984ff119ab5d6a53c82553945
Nathan Maher <nathanmaher@MacBook-Pro.local>
2025-05-18 21:24:04 -0400

    testing static

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Login />} />
+//         <Route path='/callback' element={<Callback />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/callback' element={<Callback />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1 style={{ padding: '2rem' }}>Hello from React</h1>;
 }
+
+export default App;
 
 export default App;
